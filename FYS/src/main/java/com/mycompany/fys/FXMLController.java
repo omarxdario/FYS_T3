91f5e1f322e4a627c19841b8ca586256cc0365cc
Shabier <32566872+Shabier@users.noreply.github.com>
2017-11-07 04:07:20 +0100

    Merged with MainApp & fixed switching scenes

--- FYS/src/main/java/com/mycompany/fys/FXMLController.java
+++ FYS/src/main/java/com/mycompany/fys/FXMLController.java
@@ -1,88 +1,136 @@
 package com.mycompany.fys;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class FXMLController implements Initializable {
-    
-        
-    
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-    }
 
-  @FXML
-    public void testFunction(ActionEvent event) throws IOException 
-    {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Scene_1.fxml"));
+public class FXMLController extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
-        Stage stage = new Stage();
         
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
     }
+
+        @FXML
+    public void openHome(ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    
     
     @FXML
-    public void backToContactAction(ActionEvent event) throws IOException {
+    public void testFunction(ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene_1.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-        Parent backToContact = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Credits.fxml"));
 
-        Scene contactScene = new Scene(backToContact);
-        contactScene.getStylesheets().add("/styles/Styles.css");
+      @FXML
+    public void openSubmitAction(ActionEvent event) throws IOException {
 
-        Stage creditStage = new Stage();
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Submit.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+       @FXML
+    public void openStatisticsAction(ActionEvent event) throws IOException {
 
-        creditStage.setTitle("Contact");
-        creditStage.setScene(contactScene);
-        creditStage.show();
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Statistics.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+        @FXML
+    public void openSeachLugAction(ActionEvent event) throws IOException {
+
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SearchLug.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @FXML
+    public void openCreditsAction(ActionEvent event) throws IOException {
+
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Credits.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void backToCreditsAction(ActionEvent event) throws IOException {
+
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Credits.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     public void openContact(ActionEvent event) throws IOException {
 
-        Parent openContact = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Contact.fxml"));
-
-        Scene contactScene = new Scene(openContact);
-        contactScene.getStylesheets().add("/styles/Styles.css");
-
-        Stage creditStage = new Stage();
-
-        creditStage.setTitle("Contact");
-        creditStage.setScene(contactScene);
-        creditStage.show();
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Contact.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     public void openFAQ(ActionEvent event) throws IOException {
 
-       Parent openFAQ = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/FAQ.fxml"));
-
-        Scene FAQScene = new Scene(openFAQ);
-        FAQScene.getStylesheets().add("/styles/Styles.css");
-
-        Stage FAQStage = new Stage();
-
-        FAQStage.setTitle("Contact");
-        FAQStage.setScene(FAQScene);
-        FAQStage.show();
-
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FAQ.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
