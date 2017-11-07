0b5e32b6810fad50422524578af502f988c01a9c
Shabier <32566872+Shabier@users.noreply.github.com>
2017-11-07 14:50:59 +0100

    Added ESC key action to every scene
    Pressing the ESCAPE button will bring you back to the Homepage regardless of where you are.

--- FYS/src/main/java/com/mycompany/fys/FXMLController.java
+++ FYS/src/main/java/com/mycompany/fys/FXMLController.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -16,7 +19,7 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class FXMLController extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
@@ -30,92 +33,180 @@ public class FXMLController extends Application {
     @FXML
     public void openHome(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
 
     @FXML
     public void openSubmitAction(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Submit.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
-    
+
     @FXML
     public void openStatisticsAction(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Statistics.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
-    
+
     @FXML
     public void openSeachLugAction(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/SearchLug.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
-    
+
     @FXML
     public void openCreditsAction(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Credits.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
 
     @FXML
     public void backToCreditsAction(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Credits.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
 
     @FXML
     public void openContact(ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Contact.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
 
     @FXML
-    public void openFAQ(ActionEvent event) throws IOException {
+    public void openFAQ(final ActionEvent event) throws IOException {
         Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FAQ.fxml"));
+        final Parent home = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
+        final Scene hScene = new Scene(home);
+        final Stage stage = (Stage) node.getScene().getWindow();
+        final Parent root = FXMLLoader.load(getClass().getResource("/fxml/FAQ.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ESCAPE) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    stage.setScene(hScene);
+                }
+            }
+        });
     }
-    
+
     @FXML
     public void printPDFFile(ActionEvent event) throws IOException {
         try {
             String fileName = "PDFoutput.pdf";
-            
+
             PDDocument doc = new PDDocument();
             PDPage page = new PDPage();
             doc.addPage(page);
-            
+
             PDPageContentStream content = new PDPageContentStream(doc, page);
 
             content.beginText();
@@ -144,5 +235,4 @@ public class FXMLController extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
 }
